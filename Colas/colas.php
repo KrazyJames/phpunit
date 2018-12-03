33c002d99a5d80be96c0ab851e661f5bc4b8e943
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2018-12-02 20:02:53 -0700

    Creado colas.php

---
+++ Colas/colas.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Colas
+ *
+ * Estructura de datos primero en entrar, primero en salir
+ */
+class cola
+{
+
+    /**
+     * Items de la cola
+     * @var array
+     */
+    protected $items = [];
+
+    /**
+     * Agregar un item al final de la cola
+     *
+     * @param mixed $item El item
+     */
+    public function push($item)
+    {
+        $this->items[] = $item;
+    }
+
+    /**
+     * Eliminar el item que encabeza la cola
+     *
+     * @return mixed El item
+     */
+    public function pop()
+    {
+        return array_pop($this->items);
+    }
+
+    /**
+     * Obtener el número total de items en la cola
+     *
+     * @return integer El número de items
+     */
+    public function getCount()
+    {
+        return count($this->items);
+    }
+}
